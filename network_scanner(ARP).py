7a813d0524d02f11f503e05653f91ae53c0b2a10
Ahmed Ali Elghamaz <74436594+ahmedali777-coder@users.noreply.github.com>
2024-10-08 23:52:47 +0300

    network_scanner(ARP).py

--- network_scanner(ARP).py
+++ network_scanner(ARP).py
@@ -1,17 +1,20 @@
 
 from scapy.all import ARP,Ether ,srp
 import time
-
+#ip's range network to scan
 ip="192.168.1.1/24"
-
 count=0
+ip=[]
+mac=[]
 
+#prepare arp protocol with range of network ips and broadcast mac
 arp_req= ARP(pdst=ip)
 broadcast= Ether(dst="ff:ff:ff:ff:ff:ff")
 arp_broadcast= broadcast/arp_req
-ip=[]
-mac=[]
+
+#loop more than 1 as devices not always reply they may busy 
 while count < 4 :
+    #sending arp requests by srp 
     result= srp(arp_broadcast,timeout=0,verbose=True)[0]
     if len(result)== 0:
         continue
@@ -21,6 +24,7 @@ while count < 4 :
       
     count+=1
     time.sleep(10)
+#remove duplicate ips & macs
 updated_ip=list(set(ip))
 updated_mac=list(set(mac))
 print("\n")
